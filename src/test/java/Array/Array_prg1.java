32fcf436b142ba8822b322422a7ea5ed02099ca9
amala9116 <agolapu.amala@gmail.com>
2023-10-04 20:25:36 +0530

    new package added by amala

---
+++ src/test/java/Array/Array_prg1.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Array_prg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []a= new int[3];
+		   a[0] =20;
+		   a[1] =40;
+		    a[2] =60;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		
+	}
+
+}
